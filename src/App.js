3c449d051dc6dd7be0f4fe2e3dde1106a142c9e6
ZyrenLe <80600187+ZyrenLe@users.noreply.github.com>
2022-11-15 18:16:33 +0100

    Add function for state changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,56 @@ import logo from './logo.svg';
 import './App.css';
 import ProductList from './components/products/product_list';
 import OrderButtons from './components/order/buttons';
+import { useState } from 'react';
 
+      // <ProductList />
+      // <OrderButtons />
 function App() {
+  const produkt_liste = [
+    {
+        id : 1,
+        name : "Glühwein",
+        price : "6,00",
+        count : 0
+    },
+    {
+      id : 2,
+      name : "Cider",
+      price : "5,00",
+      count : 3
+    },
+    {
+      id : 3,
+      name : "Glühwein mit Schuss",
+      price : "7,00",
+      count : 1
+    }
+  ]
+  const [bestellung,setBestellung] = useState(produkt_liste);
+
+  function test() {
+    return console.log(bestellung);
+  }
+
+  function changeState() {
+    setBestellung(
+      bestellung.map( (item)=>{
+        if (item.count !== 0){
+          const id = item.id;
+          const count = item.count;
+        }
+        return item;
+      })
+    )
+    return console.log(bestellung);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-      <ProductList />
-      <OrderButtons />
+        <button onClick={test}>test</button>
+        <button onClick={changeState}>change</button>
       </header>
     </div>
   );
