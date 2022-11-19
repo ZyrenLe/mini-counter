e5f49e068b72e3d5c32ae667fa93361b87ae51af
ZyrenLe <80600187+ZyrenLe@users.noreply.github.com>
2022-11-19 16:11:07 +0100

    Added Kinderpunsch and moved beers to top in config

--- src/mock/config.js
+++ src/mock/config.js
@@ -1,78 +1,83 @@
 
 
 export const config = [{
-        count: 0,
-        id: "8",
-        name: "Wolters",
-        price: "2,70"
-    }, {
-        count: 0,
-        id: "9",
-        name: "Wolters AKF",
-        price: "2,00"
-    }, {
-        count: 0,
-        id: "10",
-        name: "Heineken",
-        price: "2,00"
-    }, {
-        count: 0,
-        id: "1",
-        name: "Glühwein",
-        price: "3,50"
-    }, {
-        count: 0,
-        id: "2",
-        name: "Glühwein mit Schuss",
-        price: "4,50"
-    }, {
-        count: 0,
-        id: "3",
-        name: "Cider",
-        price: "3,50"
-    }, {
-        count: 0,
-        id: "4",
-        name: "Kaffee",
-        price: "2,00"
-    }, {
-        count: 0,
-        id: "5",
-        name: "Kakao",
-        price: "2,00"
-    }, {
-        count: 0,
-        id: "6",
-        name: "Tee",
-        price: "2,00"
-    }, {
-        count: 0,
-        id: "7",
-        name: "Cola, Fanta, Sprite",
-        price: "2,00"
-    }, {
-        count: 0,
-        id: "11",
-        name: "Hot Dog",
-        price: "3,00"
-    }, {
-        count: 0,
-        id: "12",
-        name: "Banh Mi (klein)",
-        price: "3,50"
-    }, {
-        count: 0,
-        id: "13",
-        name: "Banh Mi (groß)",
-        price: "7,00"
-    }, {
-        count: 0,
-        id: "14",
-        name: "Suppe",
-        price: "4,00"
-    }, {
-        count: 0,
-        id: "15",
-        name: "Hähnchen Spieß",
-        price: "2,00"
-    }]
+	"count": 0,
+	"id": "1",
+	"name": "Wolters",
+	"price": "2,70"
+}, {
+	"count": 0,
+	"id": "2",
+	"name": "Wolters AKF",
+	"price": "2,00"
+}, {
+	"count": 0,
+	"id": "3",
+	"name": "Heineken",
+	"price": "2,00"
+}, {
+	"count": 0,
+	"id": "4",
+	"name": "Glühwein",
+	"price": "3,50"
+}, {
+	"count": 0,
+	"id": "5",
+	"name": "Glühwein mit Schuss",
+	"price": "4,50"
+}, {
+	"count": 0,
+	"id": "6",
+	"name": "Cider",
+	"price": "3,50"
+}, {
+	"count": 0,
+	"id": "7",
+	"name": "Kinderpunsch",
+	"price": "2,50"
+}, {
+	"count": 0,
+	"id": "8",
+	"name": "Kaffee",
+	"price": "2,00"
+}, {
+	"count": 0,
+	"id": "9",
+	"name": "Kakao",
+	"price": "2,00"
+}, {
+	"count": 0,
+	"id": "10",
+	"name": "Tee",
+	"price": "2,00"
+}, {
+	"count": 0,
+	"id": "11",
+	"name": "Cola, Fanta, Sprite",
+	"price": "2,00"
+}, {
+	"count": 0,
+	"id": "12",
+	"name": "Hot Dog",
+	"price": "3,00"
+}, {
+	"count": 0,
+	"id": "13",
+	"name": "Banh Mi (klein)",
+	"price": "3,50"
+}, {
+	"count": 0,
+	"id": "14",
+	"name": "Banh Mi (groß)",
+	"price": "7,00"
+}, {
+	"count": 0,
+	"id": "15",
+	"name": "Suppe",
+	"price": "4,00"
+}, {
+	"count": 0,
+	"id": "16",
+	"name": "Hähnchen Spieß",
+	"price": "2,00"
+}]
